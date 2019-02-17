e9370e3df5b885d9b71731154cefff943ec5057e
LuisCasas <sc.luis@gmail.com>
2019-02-17 22:23:22 +0000

    Refactoring to use React Class and state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            lat: null
+        };
+
+        window.navigator.geolocation.getCurrentPosition((position) => {
+            this.setState({lat: position.coords.latitude});
+        }, (error) => {
+            console.log(error);
+        });       
+    };
+
+    render(){
+        return (
+            <div>Latitude: {this.state.lat}</div>
+        );
+    };
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
